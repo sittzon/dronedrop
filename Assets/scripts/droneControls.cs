c9e7b3f806234f3801332594fc85383b7426da58
Magnus Stigson <stigson87@gmail.com>
2019-10-22 06:05:29 +0200

    Adding Assets, Projectsettings and Packages. Project runnable on iPhone SE.

---
+++ Assets/scripts/droneControls.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class droneControls : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        print("rigidbody: " + rb.ToString());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            rb.AddForce(new Vector2(0.0f, 1.0f));
+        }
+    }
+}
---
+++ Assets/scripts/upButton.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class upButton : MonoBehaviour
+{
+    GameObject drone;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        drone = GameObject.Find("drone");
+        print(drone.ToString());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.touchCount > 0)
+        {
+            if (Input.touchCount == 1)
+            {
+                Touch touch = Input.GetTouch(0);
+                Vector2 pos = touch.position;
+
+                print("Touch position: " + pos);
+            }
+        }
+    }
+}
